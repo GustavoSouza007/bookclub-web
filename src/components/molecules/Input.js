909ee6f09ccc3eaad426b07e606d175b9f6c0159
GustavoSouza007 <gustavosouzatrindade10@gmail.com>
2024-05-03 16:55:18 -0300

    feat:  implemented yup and formik at the login form

--- src/components/molecules/Input.js
+++ src/components/molecules/Input.js
@@ -6,43 +6,54 @@ import {
 } from '@chakra-ui/react'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 import { useState } from 'react'
+import { Text } from 'components/atoms'
 
 export const Input = (props) => (
-  <ChakraInput
-    h="56px"
-    focusBorderColor="brand.primary"
-    placeholder={props.placeholder}
-    {...props}
-  />
+  <>
+    <ChakraInput
+      h="56px"
+      focusBorderColor="brand.primary"
+      placeholder={props.placeholder}
+      {...props}
+    />
+    {props.error && <Text color="red">{props.error}</Text>}
+  </>
 )
 
-Input.Password = (props) => {
+Input.Password = ({ value, onChange, id, name, ...props }) => {
   const [show, setShow] = useState(false)
   const handleClick = () => setShow(!show)
   return (
-    <InputGroup h="56px" size="md" {...props}>
-      <Input
-        focusBorderColor="brand.primary"
-        pr="4.5rem"
-        type={show ? 'text' : 'password'}
-        placeholder={props.placeholder}
-      />
-      <InputRightElement h="100%">
-        <Button
-          bg="transparent"
-          _hover={{ bg: 'transparent' }}
-          h="1.75rem"
-          size="sm"
-          onClick={handleClick}
-        >
-          {show ? (
-            <ViewOffIcon boxSize="20px" color="brand.primary" />
-          ) : (
-            <ViewIcon boxSize="20px" color="brand.primary" />
-          )}
-        </Button>
-      </InputRightElement>
-    </InputGroup>
+    <>
+      <InputGroup h="56px" size="md" {...props}>
+        <Input
+          id={id}
+          name={name}
+          value={value}
+          onChange={onChange}
+          focusBorderColor="brand.primary"
+          pr="4.5rem"
+          type={show ? 'text' : 'password'}
+          placeholder={props.placeholder}
+        />
+        <InputRightElement h="100%">
+          <Button
+            bg="transparent"
+            _hover={{ bg: 'transparent' }}
+            h="1.75rem"
+            size="sm"
+            onClick={handleClick}
+          >
+            {show ? (
+              <ViewOffIcon boxSize="20px" color="brand.primary" />
+            ) : (
+              <ViewIcon boxSize="20px" color="brand.primary" />
+            )}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      {props.error && <Text color="red">{props.error}</Text>}
+    </>
   )
 }
 
--- src/modules/unauthenticated/screens/Login/index.js
+++ src/modules/unauthenticated/screens/Login/index.js
@@ -1,9 +1,27 @@
 import { Flex, Image } from '@chakra-ui/react'
 import { Text, Input, Link, Button } from 'components'
 import { useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export const LoginScreen = () => {
   const navigate = useNavigate()
+  const { handleSubmit, handleChange, values, errors } = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Email inválido')
+        .required('Email é obrigatório.'),
+      password: Yup.string()
+        .min(6, 'Senha deve ter ao menos 6 caracteres.')
+        .required('Senha é obrigatório.')
+    }),
+    onSubmit: (data) => {}
+  })
+
   return (
     <Flex w="100vw" h="100vh" flexDir="row">
       <Flex
@@ -17,14 +35,32 @@ export const LoginScreen = () => {
         <Flex w={['100%', '416px', '100%', '416px']} flexDir="column">
           <Image w="160px" h="48px" src="/img/logo.svg" alt="BookClub Logo" />
           <Text.ScreenTitle mt="48px">Login</Text.ScreenTitle>
-          <Input mt="24px" placeholder="Email" />
-          <Input.Password mt="16px" placeholder="Senha" />
+          <Input
+            id="email"
+            name="email"
+            value={values.email}
+            mt="24px"
+            placeholder="Email"
+            onChange={handleChange}
+            error={errors.email}
+          />
+          <Input.Password
+            id="password"
+            name="password"
+            value={values.password}
+            mt="16px"
+            placeholder="Senha"
+            onChange={handleChange}
+            error={errors.password}
+          />
           <Flex w="100%" justifyContent="flex-end" mt="8px">
             <Link onClick={() => navigate('/forgot-password')}>
               Esqueceu sua senha?
             </Link>
           </Flex>
-          <Button mt="24px">Entrar</Button>
+          <Button onClick={handleSubmit} mt="24px">
+            Entrar
+          </Button>
           <Flex flexDir="column" alignItems="center" mt="48px">
             <Text color="brand.greyDark">Não possui uma conta?</Text>
             <Link.Action
